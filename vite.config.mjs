3bd1d4c30c83e17cf187f2c5e717ebf357b4efa1
Nico Billiere <nico.billiere@gmail.com>
2025-08-27 17:11:13 +0400

    commit auto git page

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig(({ mode }) => ({
-    base: mode === "production" ? "/portfolio-nicolas/" : "/",
+    base: mode === "production" ? "/Projet-Portfolio-Nicolas/" : "/",
     plugins: [react()],
     resolve: {
         alias: {
